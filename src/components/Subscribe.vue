dbd2b09b37f46e6c42b21a1c9d110ed3d7a08123
kelly <kellychen881110@gmail.com>
2026-01-08 21:14:44 +0800

    Add Subscribe component

---
+++ src/components/Subscribe.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="Subscribe">
+        <h1>Subscribe</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Subscribe'
+}
+</script>
+
+<style>
+.Subscribe{
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
